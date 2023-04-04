de15f3f4ef0ee8bb0ceaae712b1c79953b94c604
veggielane <chris@lane.org.uk>
2023-04-04 18:55:58 +0100

    init

---
+++ GerberWrapper/Cairo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerberWrapper
+{
+    public static class Cairo
+    {
+        private const string LIBRARY_PATH = "libcairo-2.dll"; // Change this to the path of the Gerbv library on your system
+        [DllImport(LIBRARY_PATH, EntryPoint = "cairo_surface_write_to_png_stream")]
+        public static extern int cairo_surface_write_to_png_stream(IntPtr surface, IntPtr write_func, IntPtr closure);
+    }
+}
---
+++ GerberWrapper/Gerbv.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+namespace GerberWrapper
+{
+    public static class Gerbv
+    {
+        private const string LIBRARY_PATH = "libgerbv-1.dll"; // Change this to the path of the Gerbv library on your system
+
+        [DllImport(LIBRARY_PATH, EntryPoint = "gerbv_create_project")]
+        public static extern IntPtr gerbv_create_project();
+
+        [DllImport(LIBRARY_PATH, EntryPoint = "gerbv_destroy_project")]
+        public static extern int gerbv_destroy_project(IntPtr project);
+
+        [DllImport(LIBRARY_PATH, EntryPoint = "gerbv_open_layer_from_filename")]
+        public static extern IntPtr gerbv_open_layer_from_filename(IntPtr project, string filename);
+
+
+        [DllImport(LIBRARY_PATH, EntryPoint = "gerbv_export_png_file_from_project_autoscaled")]
+        public static extern int gerbv_export_png_file_from_project_autoscaled(IntPtr project, int width, int height, string filename);
+
+
+        [DllImport(LIBRARY_PATH, EntryPoint = "gerbv_export_svg_file_from_project_autoscaled")]
+        public static extern int gerbv_export_svg_file_from_project_autoscaled(IntPtr project, string filename);
+
+        [DllImport(LIBRARY_PATH, EntryPoint = "gerbv_export_autoscale_project")]
+        public static extern IntPtr gerbv_export_autoscale_project(IntPtr project);
+
+        [DllImport(LIBRARY_PATH, EntryPoint = "gerbv_export_svg_file_from_project")]
+        public static extern int gerbv_export_svg_file_from_project(IntPtr project, IntPtr renderInfo, string filename);
+
+
+    }
+}
